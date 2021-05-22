2ed3029edcd9871603e7857f244e47d36ec92427
Yurii <yurii.kryvko@gmail.com>
2021-05-22 12:43:45 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -37,3 +37,39 @@ $(".tab").click(function() {
 	$(".tab").removeClass("active").eq($(this).index()).addClass("active");
 	$(".tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
+
+//header fixed
+$(function() {
+  const header = $('.header__top-header');
+  const slider = $('.slider');
+  const sliderHeight = slider.height();
+  const hederHeight = header.height(); // вычисляем высоту шапки
+  const fixedBlockHeighr = sliderHeight + hederHeight;
+  console.log(fixedBlockHeighr);
+   
+  $(window).scroll(function() {
+    console.log($(this).scrollTop())
+    if($(this).scrollTop() > 75) {
+     header.addClass('header__top-header__fixed');
+     $('body').css({
+        'paddingTop': hederHeight+'px' // делаем отступ у body, равный высоте шапки
+     });
+    } else {
+     header.removeClass('header__top-header__fixed');
+     $('body').css({
+      'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+     })
+    }
+    if($(this).scrollTop() > 160) {
+     slider.addClass('slider__fixed');
+     $('.main-content').css({
+      'paddingTop': '415px' // делаем отступ у body, равный высоте шапки
+   });
+    } else {
+      slider.removeClass('slider__fixed');
+      $('.main-content').css({
+        'paddingTop': 0 // делаем отступ у body, равный высоте шапки
+     });
+    }
+  });
+ });
